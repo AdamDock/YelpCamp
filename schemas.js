b0f4be94b27a1f259e0a2932c728efded0b7240c
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2024-01-15 16:53:58 -0700

    make it better

--- schemas.js
+++ schemas.js
@@ -1,42 +1,20 @@
-const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
-
-const extension = (joi) => ({
-    type: 'string',
-    base: joi.string(),
-    messages: {
-        'string.escapeHTML': '{{#label}} must not include HTML!'
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value) return helpers.error('string.escapeHTML', { value })
-                return clean;
-            }
-        }
-    }
-});
-
-const Joi = BaseJoi.extend(extension)
+const Joi = require('joi');
+const { number } = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required().escapeHTML(),
+        title: Joi.string().required(),
         price: Joi.number().required().min(0),
-        location: Joi.string().required().escapeHTML(),
-        description: Joi.string().required().escapeHTML()
-    }).required(),
-    deleteImages: Joi.array()
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
 });
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required().escapeHTML()
+        body: Joi.string().required()
     }).required()
 })
 
--- seeds/index.js
+++ seeds/index.js
@@ -21,33 +21,17 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
+        const image1 = 'https://source.unsplash.com/collection/483251';
         const camp = new Campground({
-            //YOUR USER ID
-            author: '5f5c330c2cd79d538f2c66d9',
+            author: '65a1bfc5123cc8c056360e1c',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: image1,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price,
-            geometry: {
-                type: "Point",
-                coordinates: [
-                    cities[random1000].longitude,
-                    cities[random1000].latitude,
-                ]
-            },
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
-                },
-                {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
-                }
-            ]
+            price
         })
         await camp.save();
     }
